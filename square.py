e0830401370a12530c8d8c8fbe3617614e3a3924
Satwik Mishra <satwik.mishra@Admin2s-MacBook-Air.local>
2023-06-12 16:36:10 +0530

    this is my first commit from the other branch

--- square.py
+++ square.py
@@ -1,9 +1,13 @@
 def square(x):
      """ this is to learn the pull command for the first time
-         ** has been used
+         ** has been used i am doing this in another branch
         this is my code
      """
      return(x ** 2)
+
+
+
+
 
 
 print(square(3))
